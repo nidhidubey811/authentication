d99eb5d230596cd42d8fbab4ad107f7bbb5d74d1
nidhidubey811 <73709657+nidhidubey811@users.noreply.github.com>
2025-07-26 18:18:44 +0530

    Update index.js

--- Ath_executive-main/index.js
+++ Ath_executive-main/index.js
@@ -7,3 +7,22 @@ import App from './App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import SplashScreen from 'react-native-splash-screen';
+import AppNavigator from './AppNavigator';
+
+enableScreens();
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
+export default App;
